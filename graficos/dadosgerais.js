6946910ce64d060f79ce0fbfba97ce14fdc33d6f
bibia <134613103+BIALINDAA@users.noreply.github.com>
2024-09-27 07:44:48 -0300

    Add files via upload

--- graficos/dadosgerais.js
+++ graficos/dadosgerais.js
@@ -1,10 +1,19 @@
-const url = 'https://raw.githubusercontent.com/BIALINDAA/2024-API-ex/refs/heads/main/grafico'
+const url = 'https://raw.githubusercontent.com/BIALINDAA/2024-API-ex/refs/heads/main/grafico';
 
-async function vizualizarInformaçoes() {
-    const res = await fetch(url)
-    const dados = await res.json()
+async function vizualizarInformacoes() {
+    const res = await fetch(url);
+    const dados = await res.json();
+    const pratoMaisVendido = dados.pratos[0].nome;
+    const ingredientePrincipal = dados.pratos[0].ingrediente_popular;
 
-    console.log(dados.titulo)
+    console.log(ingredientePrincipal);
+
+    const paragrafo = document.createElement('p');
+    paragrafo.classList.add('caixa-grafico__texto');
+    paragrafo.innerHTML = `Em busca de descobrir os pratos mais vendidos em 2023, foi feita uma série de pesquisas em diferentes fontes. Com o auxílio da IA do Google, foi possível estimar que o prato mais vendido foi <span>${pratoMaisVendido}</span> com um total de aproximadamente <span>${ingredientePrincipal}</span> nas plataformas de delivery.`;
+
+    const caixa = document.getElementById('caixa-grafico');
+    caixa.appendChild(paragrafo);
 }
-    
-vizualizarInformaçoes()
+
+vizualizarInformacoes();
